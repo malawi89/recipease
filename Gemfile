2ba240bc588bb34c9310c543fc01b55d0c133e09
Dave M <davidkmorley@gmail.com>
2021-11-19 17:47:03 +0000

    Add models and seeds

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ end
 
 group :development do
   gem 'listen', '~> 3.3'
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating Recipes..."
+3.times do
+  Recipe.create(name: Faker::Food.dish, meal: "Dinner", url: "www.test.com")
+end
+
+puts "Done"
+puts "Creating Ingredients..."
+15.times do
+  Ingredient.create(name: Faker::Food.ingredient)
+end
+
+puts "Done!"
+puts "Creating Recipe Ingredients..."
+Recipe.all.each do |recipe|
+  ingredient = Faker::Food.measurement
+
+  4.times do
+    RecipeIngredient.create(recipe: recipe, ingredient: Ingredient.all.sample, amount: ingredient.split(' ').first, unit: ingredient.split(' ').last)
+  end
+end
+
+puts "Done!!!"
